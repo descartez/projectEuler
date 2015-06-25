8d1d4a36801426d6d8ed1cbd77308db465e74997
descartez <rvanstong@gmail.com>
2015-06-25 16:23:10 -0700

    cleans up initial solution

--- js/multiplesOf3And5.js
+++ js/multiplesOf3And5.js
@@ -1,10 +1,20 @@
 // https://projecteuler.net/problem=1
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
-var finalSum = 0
-for (i=0; i < 1000; i++) {
-  if (i%3 === 0 || i%5 === 0) {
-    finalSum = finalSum + i;
-  }
-};
-console.log(finalSum);
+
+// Initial solution uses simple loop. If the number gets too big, it could take too much power.
+
+var sumOfThreeFives = function(upperBound) {
+  var finalSum = 0
+  for (i=0; i < upperBound; i++) {
+    if (i%3 === 0 || i%5 === 0) {
+      finalSum = finalSum + i;
+    }
+  };
+  console.log(finalSum);
+}
+
+sumOfThreeFives(1000);
+
+
+// Better solution uses math!

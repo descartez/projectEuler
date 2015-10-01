f04542b8833c65f857125d79babbf4456b09c20b
descartez <rvanstong@gmail.com>
2015-10-01 09:27:09 -0700

    turns out I had the solution, was a copy past problem;

--- ruby/self_powers.rb
+++ ruby/self_powers.rb
@@ -2,7 +2,7 @@
 
 # Find the last ten digits of the series, 1**1 + 2**2 + 3**3 + ... + 1000**1000.
 
-
+# BRUTE FORCE SOLUTION
   def nth_power(num)
     return num**num
   end
@@ -12,9 +12,9 @@
     if start < endpt
       start += 1
       sum += nth_power(start)
-      # puts "start: #{start}"
-      # puts "sum: #{sum}"
-      # puts line
+      puts "start: #{start}"
+      puts "sum: \n #{sum}"
+      puts line
       powers_generator(start, endpt, sum)
     else
       return sum
@@ -24,4 +24,5 @@ puts "TESTS"
 puts "Works up to 10: #{powers_generator(0, 10, 0) == 10405071317}"
 puts "Running to 1000"
 p powers_generator(0, 1000, 0)
+
 
